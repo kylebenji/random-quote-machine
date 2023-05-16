e09f2cfe62e26e7d8d336b62548f1947f38837cf
kylebenji <127806283+kylebenji@users.noreply.github.com>
2023-05-16 08:31:19 -0700

    adding basic styling and functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const quotes = [
+  {
+    text: "To be or not to be, that is the question.",
+    author: "Shakepeare",
+  },
+  {
+    text: "Never learn to do anything: if you don't learn, you'll always find someone else to do it for you.",
+    author: "Mark Twain",
+  },
+  {
+    text: "Power consists in one's capacity to link his will with the purpose of others, to lead by reason and a gift of cooperation.",
+    author: "Woodrow Wilson",
+  },
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.getQuote();
+    this.randomQuote = this.randomQuote.bind(this);
+  }
+
+  getQuote() {
+    const ind = Math.trunc(Math.random() * quotes.length);
+    return {
+      quote: quotes[ind].text,
+      author: quotes[ind].author,
+    };
+  }
+
+  randomQuote() {
+    this.setState(this.getQuote());
+  }
+
+  render() {
+    return (
+      <div id="quote-box" className="text-center border border-primary rounded">
+        <h1 className="p-3">Quotes</h1>
+        <div className="p-2">
+          <p id="text">{this.state.quote}</p>
+          <p id="author">-{this.state.author}</p>
+        </div>
+        <button id="new-quote" className="m-2" onClick={this.randomQuote}>
+          New Quote
+        </button>
+        <a id="tweet-quote" className="m-2" href="twitter.com/intent/tweet">
+          Tweet Quote
+        </a>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
